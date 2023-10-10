6182be59b6d59fb3ba6fba7119020ee5d6d82e21
Raymin <raymin_cabrera23@live.com>
2023-10-09 20:10:44 -0400

    second change

--- 15-sentence-mapper.js
+++ 15-sentence-mapper.js
@@ -26,8 +26,35 @@ console.log(result2); // 'ths s prtty cl rght'
 *******************************************************************************/
 
 let sentenceMapper = function (sentence, cb) {
-  // Your code here 
+  // Your code here
+    let result;
+  let words = sentence.split(" ");
+
+     result = words.map(cb)
+
+
+
+
+  return result.join(" ");
 };
+let result1 = sentenceMapper("what is the answer?", function(word) {
+  return word.toUpperCase() + "!";
+});
+console.log(result1); // 'WHAT! IS! THE! ANSWER?!'
+
+let removeVowels = function(word) {
+  let newWord = "";
+  for (let i = 0; i < word.length; i++) {
+      let char = word[i];
+      if (!"aeiou".includes(char)) {
+          newWord += char;
+      }
+  }
+  return newWord;
+};
+
+let result2 = sentenceMapper("this is pretty cool right", removeVowels);
+console.log(result2); // 'ths s prtty cl rght'
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
