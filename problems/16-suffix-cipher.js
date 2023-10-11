d54862dda3d8161f158adbf9764c3a69bcd158a9
Raymin <raymin_cabrera23@live.com>
2023-10-10 20:56:46 -0400

    16

--- problems/16-suffix-cipher.js
+++ problems/16-suffix-cipher.js
@@ -32,8 +32,61 @@ console.log(suffixCipher('incremental progress is very instrumental', cipher2));
 *******************************************************************************/
 
 function suffixCipher(sentence, cipher) {
-  // Your code here 
+  // Your code here
+  let words = sentence.split(" ");
+  
+  // for (let i = 0; i < words.length; i++) {
+  //   for (let c in cipher) {
+  //     if (words[i].endsWith(c)) {
+  //         newArr.push(cipher[c](words[i]))
+  //     }
+  //   }
+  //   newArr.push(words[i])
+
+  // }
+  // return newArr.join(" ")
+  let wordCipher = [];
+  for(let i = 0; i < words.length; i++) {
+    // apply Cipher func to words[i] and push value to emtpy array
+    for(let c in cipher) {
+        if(words[i].endsWith(c)) {
+            words[i] = cipher[c](words[i]);
+            break;
+        }
+    }
+    wordCipher.push(words[i])
+  }
+  return wordCipher.join(" ")
+
+
+
+
+
+
+  return newArr.join(" ")
 }
+
+let cipher1 = {
+  ly: function(word) {
+      return word.slice(0, -1) + 'ee';
+  },
+  ize: function(word) {
+      return word + 'r';
+  }
+};
+console.log(suffixCipher('quietly and gently visualize', cipher1));
+// quietlee and gentlee visualizer
+
+let cipher2 = {
+  tal: function(word) {
+      return word.toUpperCase();
+  },
+  s: function(word) {
+      return word + 'th';
+  }
+};
+console.log(suffixCipher('incremental progress is very instrumental', cipher2));
+// INCREMENTAL progressth isth very INSTRUMENTAL
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
